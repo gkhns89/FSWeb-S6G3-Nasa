39ea98c4b08313230a71eaa2bc50784e70b7e036
gkhns89 <gokhansisman@outlook.com>
2023-08-28 14:59:40 +0300

    Create component and get date from system.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Main from "./Components/Main";
+import Header from "./Components/Header";
 import "./App.css";
 
 function App() {
+  //Gettin actual date from system
+  const actualDate = new Date().toISOString().slice(0, 10);
+  //Set actual date to state
+  const [date, setdate] = useState(actualDate);
+
+
+  
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
+      <Main />
     </div>
   );
 }
